0e79f1e092ea5ebcc3f628f621824eafb1a6d366
Quan Vo <votrungquan99@gmail.com>
2019-12-04 21:35:00 -0500

    fix routing

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import styles from './App.module.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { AppBar } from '@clubhub/components'
-import { 
-  HomePage,
-  ClubPage,
-  NotFoundPage,
-  LoginPage,
-  RegisterPage,
-  SettingPage,
-  SearchClubPage
-} from './pages'
+import React from 'react';
+import styles from './App.module.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { AppBar } from '@clubhub/components';
+import {
+	HomePage,
+	ClubPage,
+	NotFoundPage,
+	LoginPage,
+	RegisterPage,
+	SettingPage,
+	SearchClubPage
+} from './pages';
 
 const App = () => {
-  return (
-    <div className={styles.root}>
-      <Router>
-        <AppBar />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/club/" component={SearchClubPage}/>
-          <Route path="/club/:id" component={ClubPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/setting" component={SettingPage} />
-          <Route path="*" component={NotFoundPage} />
-        </Switch>
-      </Router>
-    </div>
-  )
-}
+	return (
+		<div className={styles.root}>
+			<Router>
+				{/* <AppBar /> */}
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/club/:id' component={ClubPage} />
+					<Route exact path='/club/' component={SearchClubPage} />
+					<Route path='/login' component={LoginPage} />
+					<Route path='/register' component={RegisterPage} />
+					<Route path='/setting' component={SettingPage} />
+					<Route path='*' component={NotFoundPage} />
+				</Switch>
+			</Router>
+		</div>
+	);
+};
 
-export default App
+export default App;
